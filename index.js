5448f7abe161120b66aac33421910f9d44e8f7da
noviny <novinyarts@gmail.com>
2016-06-17 17:32:42 +1000

    removed StandardDice
    These were adding little to no value

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import { generateRoll, newRoll } from './diceLogic';
 import StaticRoll from './components/StaticRoll'
 import SimpleDie from './components/SimpleDie'
-import standardDice, { d4, d6, d8, d10, d12, d20 } from './standardDice';
 
 exports.generateRoll = generateRoll
 exports.newRoll = newRoll
 exports.StaticRoll = StaticRoll
 exports.SimpleDie = SimpleDie
-exports.standardDice = standardDice
-
-exports.d4 = d4
-exports.d6 = d6
-exports.d8 = d8
-exports.d10 = d10
-exports.d12 = d12
-exports.d20 = d20
