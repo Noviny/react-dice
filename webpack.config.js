d81b4d7f8832535a1865daf8e7b8d5d4ed7fcf94
noviny <novinyarts@gmail.com>
2016-07-02 14:43:30 +1000

    webpack and babel added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'./index.js',
+	],
+	output: {
+		path: __dirname + '/dist/',
+		filename: 'bundle.min.js',
+	},
+	plugins: [// Plugins for Webpack
+		new webpack.optimize.UglifyJsPlugin({ // Optimize the JavaScript...
+			compress: {
+				warnings: false, // ...but do not show warnings in the console (there is a lot of them)
+			},
+		}),
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react'],
+				},
+			},
+		],
+	},
+	devtool: 'source-map',
+};
